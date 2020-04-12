479df12fefb6f0e151c8010a2d77be8003a35dc7
fouedj <fouedj@hotmail.com>
2020-04-12 19:58:45 +0200

    push client

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from '@apollo/react-hooks';
 import {setContext}from 'apollo-link-context'
 
 const httpLink=createHttpLink({
-    uri:'http://localhost:5001'
+    uri:'https://shielded-tor-84925.herokuapp.com/'
 })
 const authLink=setContext(()=>{
     const token=localStorage.getItem('jwtToken')
